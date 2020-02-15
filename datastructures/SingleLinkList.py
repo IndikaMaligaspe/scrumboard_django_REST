1cd691da1342ce99976710ec7ae887a7a3216751
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-02-15 05:57:16 +0530

    init commit

---
+++ datastructures/SingleLinkList.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.nextval = None
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.headval = None
+    
+if __init__ == "__main__":
+    list = SingleLinkedList()
