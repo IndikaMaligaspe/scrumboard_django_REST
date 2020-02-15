87285310804d97e764861489ce913c60f8b5a79f
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-02-15 06:16:05 +0530

    working on Single linked list operations

--- datastructures/SingleLinkList.py
+++ datastructures/SingleLinkList.py
@@ -8,5 +8,39 @@ class SingleLinkedList:
     def __init__(self):
         self.headval = None
     
-if __init__ == "__main__":
+    
+    def traversList(self):
+        currval = self.headval;
+        while currval is not None:
+            print(currval.data)
+            currval = currval.nextval
+    
+
+    def addAtBeginning(self, newdata):
+        newNode = Node(newdata)
+        newNode.nextval = self.headval
+        self.headval = newNode
+    
+    
+    def addAtEnd(self, newdata):
+        newNode = Node(newdata)
+        if self.headval == None:
+            self.headval = newNode
+            return
+        last = self.headval
+        while (last.nextval):
+            last = last.nextval
+        last.nextval = newNode
+
+
+if __name__ == "__main__":
     list = SingleLinkedList()
+    list.headval = Node("February")
+    secondElement = Node("March")
+    list.headval.nextval = secondElement
+    thirdElement = Node("April")
+    secondElement.nextval = thirdElement
+    list.addAtBeginning("January")
+    list.addAtEnd("May")
+    list.traversList()
+
