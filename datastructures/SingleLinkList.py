6c2adce2d72e71e9556bfc7e61a45338c0161fc9
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-02-15 06:50:58 +0530

    basic operations Single Linked List done

--- datastructures/SingleLinkList.py
+++ datastructures/SingleLinkList.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, data=None):
         self.data = data
-        self.nextval = None
+        self.nextNode = None
 
 
 class SingleLinkedList:
@@ -13,12 +13,12 @@ class SingleLinkedList:
         currval = self.headval;
         while currval is not None:
             print(currval.data)
-            currval = currval.nextval
+            currval = currval.nextNode
     
 
     def addAtBeginning(self, newdata):
         newNode = Node(newdata)
-        newNode.nextval = self.headval
+        newNode.nextNode = self.headval
         self.headval = newNode
     
     
@@ -28,19 +28,65 @@ class SingleLinkedList:
             self.headval = newNode
             return
         last = self.headval
-        while (last.nextval):
-            last = last.nextval
-        last.nextval = newNode
+        while (last.nextNode):
+            last = last.nextNode
+        last.nextNode = newNode
+
+    
+    def addAt(self, atnode, newnode):
+        if atnode is None:
+            print ("At Node is not avaibla ..")
+            return
+        newNode = Node(newnode)
+        newNode.nextNode = atnode.nextNode
+        atnode.nextNode = newNode
+        
+    def popElem(self, key):
+        headVal =  self.headval
+        if headVal is None:
+            print("list empty already...")
+            return
+        if headVal.data == key:
+            self.headval  = headVal.nextNode
+            headVal = None
+            return
+        while(headVal is not None):
+            if headVal.data == key:
+                break
+            prev = headVal
+            headVal = headVal.nextNode
+
+        if (headVal is None):
+            print("Key not found...")
+            return        
+
+        prev.nextNode = headVal.nextNode
+        headVal = None
 
 
 if __name__ == "__main__":
     list = SingleLinkedList()
     list.headval = Node("February")
     secondElement = Node("March")
-    list.headval.nextval = secondElement
+    list.headval.nextNode = secondElement
     thirdElement = Node("April")
-    secondElement.nextval = thirdElement
+    secondElement.nextNode = thirdElement
+    list.traversList()
+    print("--------------------------")
+
     list.addAtBeginning("January")
+    list.traversList()
+    print("--------------------------")
+
     list.addAtEnd("May")
     list.traversList()
+    print("--------------------------")
+
+    list.addAt(thirdElement,"April 14 - New Year day")
+    list.traversList()
+    print("--------------------------")
+    list.popElem("April 14 - New Year day")
+    print("--------------------------")
+    list.traversList()
+    list.popElem("August")
 
