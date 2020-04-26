a67f829d47fdfb17184896dd2c50329e9d5693c5
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-04-26 17:33:37 +0530

    init REST API

--- REST_projects/scrum/board/views.py
+++ REST_projects/scrum/board/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import authentication, permissions, viewsets
+from .models import Sprint , Task
+from .serializer import SprintSerializer , TaskSerializer, UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class DefaultMixins(object):
+    authentication_classes = [
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    ]
+    
+    permission_classes = [permissions.IsAuthenticated,]
+    
+
+    paginate_by =  25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+
+
+class SprintViewSet(DefaultMixins, viewsets.ModelViewSet):
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
+class TaskViewSet(DefaultMixins, viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class UserViewSet(DefaultMixins, viewsets.ReadOnlyModelViewSet):
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
