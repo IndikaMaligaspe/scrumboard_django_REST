97854e4f975e4f2236430105816e44a1e39fcf05
IndikaMaligaspe <k.indika.maligaspe@gmail.com>
2020-04-30 08:53:07 +0530

    added create sprint form, have a issue with re-rendering

--- REST_projects/scrum/board/static/board/js/views.js
+++ REST_projects/scrum/board/static/board/js/views.js
@@ -12,27 +12,7 @@
         },
         getContext: function () { return{}; }
     });
-
-    var HomepageView = TemplateView.extend({
-        templateName: '#home-template',
-        initialize: function (options) {  
-            var self = this;
-            TemplateView.prototype.initialize.apply(this, arguments);
-            app.collections.ready.done(function () {
-                var end = new Date();
-                end.setDate(end.getDate() - 7);
-                end = end.toISOString().replace(/T.*/g, '');
-                app.sprints.fetch({
-                    data: {end_min: end},
-                    success: $.proxy(self.render, self)
-                });
-            });
-        },
-        getContext: function () {
-            return {sprints: app.sprints || null};
-        }
-    });
-
+    
     var FormView = TemplateView.extend({
         events: {
             'submit form': 'submit'
@@ -54,7 +34,7 @@
                 _.map(fieldErrors, appendError, this);
             }, this);
         },
-        serializeForm: function (event) {  
+        serializeForm: function (form) {  
             return _.object(_.map(form.serializeArray(), function (item){
                 return[item.name, item.value];
             }));
@@ -74,10 +54,73 @@
             }
             this.trigger('done');
             this.remove();
+        },
+        modelFailure: function (model, xhr, options) {  
+            var errors = xhr.responseJSON;            
+            this.showErrors(errors);
         }
-
     });
 
+    var NewSprintVew = FormView.extend({
+        templateName: '#new-sprint-template',
+        className: 'new-sprint',
+        events: _.extend({
+            'click button.cancel': 'done'
+        }, FormView.prototype.events),
+        submit: function (event) {
+            var self = this,
+                attributes = {};
+            FormView.prototype.submit.apply(this, arguments);
+            attributes = this.serializeForm(this.form);
+            app.collections.ready.done(function () {
+                app.sprints.create(attributes, {
+                    wait: true,
+                    success: $.proxy(self.success, self),
+                    error: $.proxy(self.modelFailure, self)      
+                });
+            });
+        },
+        success: function (model) {
+            this.done();
+            window.location.hash ='#sprint/' + model.get('id');
+        }
+    });
+
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template',
+        events: {
+            'click button.add': 'renderAddForm'
+        },
+        initialize: function (options) {  
+            var self = this;
+            TemplateView.prototype.initialize.apply(this, arguments);
+            app.collections.ready.done(function () {
+                var end = new Date();
+                end.setDate(end.getDate() - 7);
+                end = end.toISOString().replace(/T.*/g, '');
+                app.sprints.fetch({
+                    data: {end_min: end},
+                    success: $.proxy(self.render, self)
+                });
+            });
+        },
+        getContext: function () {
+            return {sprints: app.sprints || null};
+        },
+        renderAddForm: function (event) { 
+            console.log(event);
+             
+            var view = new NewSprintVew(),
+                link = $(event.currentTarget);
+            event.preventDefault()
+            link.before(view.el);
+            link.hide();
+            view.render();
+            view.on('done', function () {
+                link.show();
+            });
+        }
+    });
     var LoginView = FormView.extend({
         id: 'login',
         templateName: '#login-template',
